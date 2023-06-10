60f681401c85b215b7c147f0476906eb019b1fb0
mingfengpigeon <mingfengpigeon@gmail.com>
2023-06-10 14:42:16 +0800

    Implement Sudoku generator using backtracking algorithm

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -5,31 +5,27 @@
 #include "Sudoku.h"
 
 
-bool IsRemovable::operator()(int x, int y) const {
-    if (x == target_x and y != target_y) {
-        return true;
+std::vector<int> Sudoku::get_available_values(const int x, const int y) const {
+    const int x_begin = x / 3 * 3;
+    const int x_end = x_begin + 3;
+    const int y_begin = y / 3 * 3;
+    const int y_end = y_begin + 3;
+    std::vector<int> available_values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 9; i++) {
+        auto it = std::find(available_values.begin(), available_values.end(), board[x][i]);
+        if (it != available_values.end()) {
+            available_values.erase(it);
+        }
     }
-    if (y == target_y and x != target_x) {
-        return true;
+    for (int i = 0; i < 9; i++) {
+        auto it = std::find(available_values.begin(), available_values.end(), board[i][y]);
+        if (it != available_values.end()) {
+            available_values.erase(it);
+        }
     }
-    if (x >= target_x_begin and x < target_x_end and y >= target_y_begin and y < target_y_end and
-        x != target_x and y != target_y) {
-        return true;
-    }
-    return false;
-}
-
-std::vector<int> Sudoku::get_available_values(const int target_x, const int target_y) const {
-    IsRemovable is_removable(target_x, target_y);
-    std::vector<int> available_values;
-    for (int i = 0; i <= 9; i++) {
-        available_values.push_back(i);
-    }
-    for (int i = 0; i <= 80; i++) {
-        const int x = i % 9;
-        const int y = i / 9;
-        if (is_removable(x, y)) {
-            auto it = std::find(available_values.begin(), available_values.end(), board[y][x]);
+    for (int i = x_begin; i < x_end; i++) {
+        for (int j = y_begin; j < y_end; j++) {
+            auto it = std::find(available_values.begin(), available_values.end(), board[i][j]);
             if (it != available_values.end()) {
                 available_values.erase(it);
             }
@@ -38,16 +34,36 @@ std::vector<int> Sudoku::get_available_values(const int target_x, const int targ
     return available_values;
 }
 
-void Sudoku::display() const {
-    for (int i = 0; i <= 80; i++) {
-        const int x = i % 9;
-        const int y = i / 9;
-        if (x == 0) {
-            std::cout << std::endl;
+bool Sudoku::generate(int x, int y) {
+    if (x == 9) {
+        x = 0;
+        y++;
+    }
+    if (y == 9) {
+        return true;
+    }
+    std::vector<int> available_values(get_available_values(x, y));
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(available_values.begin(), available_values.end(), gen);
+    for (int available_value: available_values) {
+        board[x][y] = available_value;
+        if (generate(x + 1, y)) {
+            return true;
         }
-        if (x != 9) {
-            std::cout << " ";
+        board[x][y] = 0;
+    }
+    return false;
+}
+
+void Sudoku::display() {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            std::cout << board[j][i];
+            if (j < 8) {
+                std::cout << " ";
+            }
         }
-        std::cout << board[y][x];
+        std::cout << std::endl;
     }
 }
--- Sudoku.h
+++ Sudoku.h
@@ -7,75 +7,24 @@
 
 
 #include <vector>
-#include <map>
 #include <algorithm>
 #include <random>
 #include <iostream>
 
 
-class IsRemovable{
-public:
-    IsRemovable(int x, int y): target_x(x), target_y(y), target_x_begin(x / 3 * 3), target_x_end(x / 3 * 3 + 3), target_y_begin(y / 3 * 3), target_y_end(y / 3 * 3 + 3) {}
-
-    bool operator()(int x, int y) const;
-private:
-    int target_x;
-
-    int target_y;
-
-    int target_x_begin;
-
-    int target_x_end;
-
-    int target_y_begin;
-
-    int target_y_end;
-};
-
-
 class Sudoku {
 public:
     Sudoku(): board(9, std::vector<int>(9)) {
-        for (int i = 0; i <= 80; i++) {
-            const int x = i % 9;
-            const int y = i / 9;
-            IsRemovable is_removable(x, y);
-            std::map<int, int> times;
-            std::vector<int> available_values(get_available_values(x, y));
-            for (int available_value: available_values) {
-                times[available_value] = 0;
-            }
-            for (int j = 0; j <= 80; j++) {
-                const int inner_x = j % 9;
-                const int inner_y = j / 9;
-                if (is_removable(inner_x, inner_y)) {
-                    auto it = std::find(available_values.begin(), available_values.end(), board[inner_y][inner_x]);
-                    if (it != available_values.end()) {
-                        times[board[inner_y][inner_x]]++;
-                    }
-                }
-            }
-            auto it = min_element(times.begin(), times.end(), [](const auto& left, const auto& right) {
-                return left.second < right.second;
-            });
-            available_values.erase(available_values.begin(), available_values.end());
-            for (std::pair<const int, int> time: times) {
-                if (time.second == it->second) {
-                    available_values.push_back(time.first);
-                }
-            }
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            std::uniform_int_distribution<> dis(0, static_cast<int>(available_values.size()) - 1);
-            board[y][x] = available_values[dis(gen)];
-        }
-    };
+        generate(0, 0);
+    }
 
-    [[nodiscard]] std::vector<int> get_available_values(int target_x, int target_y) const;
+    [[nodiscard]] std::vector<int> get_available_values(int x, int y) const;
+
+    bool generate(int x, int y);
+
+    void display();
 
     std::vector<std::vector<int>> board;
-
-    void display() const;
 };
 
 
